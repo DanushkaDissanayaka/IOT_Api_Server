48758f5bfb8c41deb71259398e28d25e27427581
Danushka Dissanayaka <dsjayamal@gmail.com>
2021-03-06 02:15:58 +0530

    -- body parser added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser')
 
 // Api Points
 const api = require('./routes/api')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
